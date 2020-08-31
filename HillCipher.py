c04011e61e6c802735591d0a9536fc8747d18533
Abhiram2001 <65899056+Abhiram2001@users.noreply.github.com>
2020-08-31 13:24:33 +0530

    Update HillCipher.py

--- HillCipher.py
+++ HillCipher.py
@@ -1,43 +1,117 @@
 import math
 import numpy as np
+from sympy import Matrix
+from os import system, name
 
-key = input("Enter the Key:  ")
-key = key.lower()
-keylen = len(key)
-dim  = math.ceil(math.sqrt(keylen))
-# Initialization
-arr = [[1234 for i in range(dim)] for j in range(dim)]
-for i in range(0, keylen):
-    arr[i//dim][i%dim] =  ord(key[i]) - 97
+def clear(): 
+    if name == 'nt': 
+        _ = system('cls') 
+    else: 
+        _ = system('clear')
 
-for i in range(keylen, dim*dim):
-    arr[i//dim][i%dim] = (i-keylen)
+key=""
+keylen=0
+dim = 0
+arr = []
 
-pt = input("Enter the plain text :  ")
-alter = str(pt)
-ptlen = len(pt)
-add = ptlen % dim
-add = (dim - add, 0)[add == 0]
-for i  in range (0, add):
-    alter = alter + "x"
+def keyInput():
+    global key , keylen , dim ,arr
+    key = input("Enter the Key:  ")
+    key = key.lower()
+    keylen = len(key)
+    dim  = math.ceil(math.sqrt(keylen))
+    arr = [[1234 for i in range(dim)] for j in range(dim)]
 
-print()
-print("*****************************************************")
-print("The altered plain text :",alter)
-print()
-print("*****************************************************")
+    for i in range(0, keylen):
+        arr[i//dim][i%dim] =  ord(key[i]) - 97
 
-for row in arr: 
-    print(row)
+    for i in range(keylen, dim*dim):
+        arr[i//dim][i%dim] = (i-keylen)
 
-k = 0
-answer = ""
-while k<len(alter):
-    data = [[ord(alter[i+j+k]) - 97 for i in range(1)] for j in range(dim)]
-    data = np.dot(arr,data)
-    for i in range(dim):
-        answer = answer + str(chr((data[i][0]%26)+97))
-    k+=dim
-print()
-print("*****************************************************")
-print("Cipher Text : ",answer)
+    if np.linalg.det(np.array(arr)) == 0:
+        print("We Cannot decrypt with this key so Choose another")
+        print("*****************************************************")
+        keyInput()
+
+
+
+def encryption():
+    global key , keylen , dim ,arr
+    print("********************ENCRYPTION*********************************")
+    keyInput()
+    print("*****************************************************")
+    pt = input("Enter the plain text :  ").lower()
+    alter = str(pt)
+    ptlen = len(pt)
+    add = ptlen % dim
+    add = (dim - add, 0)[add == 0]
+    for i  in range (0, add):
+        alter = alter + "x"
+
+    print()
+    print("*****************************************************")
+    print("The altered plain text :",alter)
+    print()
+    print("*****************************************************")
+
+    for row in arr: 
+        print(row)
+
+    k = 0
+    answer = ""
+    while k<len(alter):
+        data = [[ord(alter[i+j+k]) - 97 for i in range(1)] for j in range(dim)]
+        data = np.dot(arr,data)
+        for i in range(dim):
+            answer = answer + str(chr((data[i][0]%26)+97))
+        k+=dim
+    print()
+    print("*****************************************************")
+    print("Cipher Text : ",answer.upper())
+
+
+
+
+def decryption():
+    global key , keylen , dim ,arr
+    print("********************DECRYPTION*********************************")
+    keyInput()
+    print("*****************************************************")
+    ct = input("Enter the Cipher text :  ").lower()
+    print()
+    print("*****************************************************")
+
+    inverseKeyMatrix = Matrix(arr).inv_mod(26)
+    np.inverseKeyMatrix = np.array(inverseKeyMatrix)
+    arr = np.array(inverseKeyMatrix)
+    for row in arr: 
+        print(row)    
+
+    k = 0
+    answer = ""
+    while k<len(ct):
+        data = [[ord(ct[i+j+k]) - 97 for i in range(1)] for j in range(dim)]
+        data = np.dot(inverseKeyMatrix,data)
+        for i in range(dim):
+            answer = answer + str(chr(int(round(data[i][0]))%26 + 97))
+        k+=dim
+    print()
+    print("*****************************************************")
+    print("Plain Text : ",answer.upper())
+
+
+
+def main():
+  ch=int(input("1.Encryption\n2.Decryption\nEnter the choice:"))
+  if ch == 1:
+      encryption()
+  elif ch == 2:
+    decryption()
+  else:
+    clear()
+    print("Wrong input Try Again")
+    print()
+    main()
+
+if __name__ == "__main__": 
+    main() 
